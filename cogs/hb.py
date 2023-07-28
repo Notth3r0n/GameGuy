5688b17975dcf90efbdbd20fa90956b47f7aa9b7
Notth3r0n <138650414+Notth3r0n@users.noreply.github.com>
2023-07-28 22:13:41 +0800

    added annotations

--- cogs/hb.py
+++ cogs/hb.py
@@ -16,31 +16,38 @@ class HorseBetting(commands.Cog):
         with open("cogs/eco.json", "r") as f:
             user_eco = json.load(f)
 
+        #check if member has account, if not make one for them
         if str(ctx.author.id) not in user_eco:
             user_eco[str(ctx.author.id)] = {}
             user_eco[str(ctx.author.id)]["Wallet"] = 50
             user_eco[str(ctx.author.id)]["Bank"] = 0
 
+        #check if bet is positive
         if bet <= 0:
             await ctx.send("Please enter a valid bet greater than zero.")
             return
 
+        check if user has enough coins
         if bet > user_eco[str(ctx.author.id)]["Wallet"]:
             await ctx.send("You don't have enough coins to place that bet.")
             return
 
+        #check if user chose a horse
         num_horses = 4
         if horse < 1 or horse > num_horses:
             await ctx.send("Please choose a valid horse number.")
             return
 
+        #randomizing winning horse
         winning_horse = random.randint(1, num_horses)
 
+        #win condition
         if horse == winning_horse:
             win_em = discord.Embed(title='Congratulations!', description=f'Horse {horse} won!', color= discord.Color.blurple())
             win_em.add_field(name='', value=f'{bet} coins have been added to your wallet')
             await ctx.send(embed=win_em)
             user_eco[str(ctx.author.id)]["Wallet"] += bet
+        #lose condition
         else:
             lose_em = discord.Embed(title='Awww!', description=f'Horse {horse} lost!', color=discord.Color.blurple())
             lose_em.add_field(name='', value=f'{bet} coins have been deducted from your wallet')
