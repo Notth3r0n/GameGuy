be76d8681b964e6de76484e62262007fc740f53b
Notth3r0n <138650414+Notth3r0n@users.noreply.github.com>
2024-04-10 14:32:45 +0800

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,18 +1,1 @@
-from flask import Flask
-from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
-#what is printed on the webserver
-def home():
-    return("Hi!")
-
-#setting up website hosting
-def run():
-  app.run(host='0.0.0.0',port=8080)
-
-#start website
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
